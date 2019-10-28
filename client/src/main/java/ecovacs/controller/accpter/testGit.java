2483a519081770b2d448693a0e2c32f172253de0
dgbtds <435959013@qq.com>
2019-10-28 22:32:55 +0800

    测试git-dev版本

---
+++ client/src/main/java/ecovacs/controller/accpter/testGit.java
@@ -0,0 +1,17 @@
+package ecovacs.controller.accpter;
+
+/**
+ * @author WuYe
+ * @vesion 1.0 2019/10/28
+ * /
+ * /**
+ * @program: parent
+ * @description: 测试版本控制
+ * @author: WuYe
+ * @create: 2019-10-28 22:31
+ **/
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("1111111111");
+    }
+}
